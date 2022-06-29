c8ff89145158ef71b30c9a3c9de6d84236c55951
Jihyo Jeon <jihyo@getofficely.com>
2022-06-29 20:49:27 +0100

    Time: 118 ms (54.30%), Space: 51.9 MB (25.20%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,14 +4,14 @@
  */
 var maxProfit = function(prices) {
     let min = prices[0];
-    let profit = 0;
+    let maxProfit = 0;
     for(let i = 1; i < prices.length; i++) {
         if (prices[i] > prices[i-1]) {
-            profit = Math.max(profit, prices[i] - min);
+            maxProfit = Math.max(maxProfit, prices[i] - min);
         } else {
             min = Math.min(min, prices[i]);;
         }
     }
 
-    return profit;
+    return maxProfit;
 };
