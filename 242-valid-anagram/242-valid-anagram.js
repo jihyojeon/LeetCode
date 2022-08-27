f9be529570ee0dc925b41aae79d5ca917d963413
Jihyo Jeon <jihyo@getofficely.com>
2022-08-27 17:34:54 +0100

    Time: 166 ms (16.62%), Space: 46.4 MB (36.21%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,5 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join() === t.split('').sort().join()
+    // return s.split('').sort().join() === t.split('').sort().join()
+    const first = s.split('').sort();
+    const second = t.split('').sort();
+    for (let i=first.length-1; i>=0; i--) {
+        if (first[i] != second[i]) {
+            return false
+        }
+        first.pop();
+        second.pop();
+    }
+    return second.length === 0? true: false;
 };
