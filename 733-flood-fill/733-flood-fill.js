fc5ff38c02f3a9f5f7ca913dbfccf6ed5dfa8fdc
Jihyo Jeon <jihyo@getofficely.com>
2022-08-27 23:38:12 +0100

    Time: 70 ms (98.14%), Space: 44.3 MB (60.07%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+const neighbours = [[-1,0], [0,-1], [0,1], [1,0]];
+var floodFill = function(image, sr, sc, color) {
+    const prevColor = image[sr][sc];
+    if (color === prevColor) {
+        return image
+    }
+    const fill = (x, y) => {
+        if (
+            !image[x] ||
+            image[x][y] === undefined ||
+            image[x][y] === color ||
+            image[x][y] !== prevColor
+        ) {
+            return;
+        }
+        image[x][y] = color;
+        neighbours.forEach(coor => {
+            fill(x+coor[0], y+coor[1]);
+        })
+    };
+    
+	fill(sr, sc);
+    
+	return image;
+    
+};
