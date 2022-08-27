f04504be12e726742716481c9d4c8be5e67f1ec0
Jihyo Jeon <jihyo@getofficely.com>
2022-08-27 14:45:59 +0100

    Time: 71 ms (97.02%), Space: 47.4 MB (30.56%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,8 +6,13 @@
 var isPalindrome = function(s) {
     const regex = new RegExp('[A-Za-z0-9]');
     const letters = s.split('').filter((l) => regex.test(l)).map(l => l.toLowerCase());
-    const input = letters.join();
-    const reversed = letters.reverse().join();
-    console.log(input, reversed);
-    return input === reversed? true: false;
+    let right = letters.length - 1;
+    for (let left=0; left<=right; left ++) {
+        if (letters[left] != letters[right]) {
+            return false;
+        } else {
+            right -= 1;
+        }
+    }
+    return true;
 };
