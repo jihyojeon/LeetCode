cc2c7f44e1cf83a4fc9443e22850e3fafcb78c9b
Jihyo Jeon <jihyo@getofficely.com>
2022-08-28 17:06:58 +0100

    Time: 115 ms (61.73%), Space: 50.3 MB (58.70%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let current = nums[0];
+    let max = nums[0];
+    
+    for (let i=1; i<nums.length; i++) {
+        current = Math.max(nums[i], nums[i] + current);
+        max = Math.max(current, max);
+    }
+    
+    return max
+};
