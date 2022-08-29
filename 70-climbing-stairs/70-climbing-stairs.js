df02f0587c129fa11ee75c8a54ede1b3764c5e1f
Jihyo Jeon <jihyo@getofficely.com>
2022-08-29 19:21:13 +0100

    Time: 81 ms (60.06%), Space: 42.6 MB (6.92%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var climbStairs = function(n) {
+    const climb = [0, 1, 2];
+    let i = 3
+    while(!climb[n]) {
+        climb.push(climb[i-1] + climb[i-2]);
+        i += 1;
+    }
+    console.log(n, climb)
+    return climb[n]
+};
