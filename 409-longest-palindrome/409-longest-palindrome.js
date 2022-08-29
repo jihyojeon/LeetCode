126efe10ceec6d700bb207c5838956e290a9eaaa
Jihyo Jeon <jihyo@getofficely.com>
2022-08-29 19:35:09 +0100

    Time: 141 ms (6.68%), Space: 44.3 MB (38.31%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.js
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    const letters = {}
-    let len = 0
+    const letters = {};
+    let len = 0;
+    let one = 0;
     s.split('').map(l => {
         if (letters[l] === 1) {
             letters[l] = 0;
             len += 2;
+            one -= 1;
         } else {
             letters[l] = 1;
+            one += 1;
         }
     })
     
-    
-    return Object.values(letters).every(el => el===0) ? len : len + 1;
+    return len + !!one;
 };
