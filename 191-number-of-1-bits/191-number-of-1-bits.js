177a6d276b43a72978f338d140b2bcca59453ea9
Jihyo Jeon <jihyo@getofficely.com>
2022-09-03 23:16:19 +0100

    Time: 68 ms (94.96%), Space: 42.1 MB (86.70%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -3,12 +3,6 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    // const arr = [0];
-    // for (let i=1; i<=n; i++) {
-    //     arr.push(arr[i&(i-1)]+1)
-    // }
-    // return arr[n]
-    
     if (n===0) {
         return 0
     }
